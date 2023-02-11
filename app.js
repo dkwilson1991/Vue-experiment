10904391d07e1bcdf8c96ae1a00978c14924c818
Keita Wilson <dkwilson1991@gmail.com>
2023-02-11 15:16:28 +0900

    random user generate

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const app = Vue.createApp({
   data() {
     return {
-      firstName: 'Keita',
-      lastName: 'Wilson',
-      email: 'dkwilson1991@gmail.com',
+      firstName: 'Make a Random',
+      lastName: 'User',
+      email: 'random@email.com',
       gender: 'male',
-      picture: 'http://randomuser.me/api/portraits/men/1.jpg',
+      picture: 'http://randomuser.me/api/portraits/men/4.jpg'
+    }
+  },
+  methods: {
+    async getUser() {
+      const res = await fetch('http://randomuser.me/api')
+      const { results } = await res.json()
+
+      this.firstName = results[0].name.first,
+      this.lastName = results[0].name.last,
+      this.email = results[0].email,
+      this.gender = results[0].gender,
+      this.picture = results[0].picture.large
     }
   }
 })

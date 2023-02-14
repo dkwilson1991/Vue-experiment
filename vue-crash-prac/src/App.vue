078e78c7dd125ccd465909d47cec748e87b8d020
Keita Wilson <dkwilson1991@gmail.com>
2023-02-14 13:28:50 +0900

    making task tracker

--- vue-crash-prac/src/App.vue
+++ vue-crash-prac/src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Header title="Task Tracker"></Header>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
